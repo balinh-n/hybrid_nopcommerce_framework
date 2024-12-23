eced44fca5fdc7ace97f1e53e6f43b433c7205cd
Linh <linhnb.0907@gmail.com>
2024-12-23 09:55:24 +0700

    commit

--- src/main/java/pageObjects/jquery/HomePageObject.java
+++ src/main/java/pageObjects/jquery/HomePageObject.java
@@ -1,5 +1,11 @@
 package pageObjects.jquery;
 
+import org.openqa.selenium.WebDriver;
+
 public class HomePageObject {
+    WebDriver driver;
+    public HomePageObject(WebDriver driver) {
+        this.driver = driver;
+    }
     
 }
